76785985ad45b8f0c1563ac389afde4b54f561b7
aaout <tiktik_suke@outlook.jp>
2020-08-16 01:16:58 +0900

    AtCoder B

---
+++ Contest/B/Palindromic.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//初見×
+//下手に文字列に変換しないこと
+int main()
+{
+    int A, B;
+    int count = 0;
+    cin >> A >> B;
+
+    for (int i = A; i <= B; i++)
+    {
+        int s = i % 10, t = i / 10000 % 10;
+        int u = i / 10 % 10, v = i / 1000 % 10;
+        if (s == t && u == v)
+            count++;
+    }
+    cout << count << endl;
+}
