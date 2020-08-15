106222928f656de8223924e975e6730f1c5aa01d
aaout <tiktik_suke@outlook.jp>
2020-08-16 01:40:12 +0900

    ignore file

---
+++ Beginner/第1章/1_12.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str = "Hello";
+
+    cout << str << endl;
+    cout << str.size() << endl;
+    cout << str.at(0) << endl;
+    cout << str.at(1) << endl;
+    cout << str.at(2) << endl;
+    cout << str.at(3) << endl;
+    cout << str.at(4) << endl;
+}
