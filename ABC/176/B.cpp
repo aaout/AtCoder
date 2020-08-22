5ed710f87cf84c53e0ad9fd709123f4c9b67edff
aaout <tiktik_suke@outlook.jp>
2020-08-23 01:46:14 +0900

    memo

--- ABC/176/B.cpp
+++ ABC/176/B.cpp
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = unsigned long long;
-ll N;
+char N[200010];
 
 //WA
-int findSumOfDigits(ll n)
-{
-    ll sum = 0;
-    while (n > 0)
-    {
-        sum += n % 10;
-        n /= 10;
-    }
-    return sum;
-}
+//数値で扱おうとするとメモリが足りないので文字列として扱う
+//わざわざ関数を用意しなくても各位の和を求めれば行ける
+// int findSumOfDigits(ll n)
+// {
+//     ll sum = 0;
+//     while (n > 0)
+//     {
+//         sum += n % 10;
+//         n /= 10;
+//     }
+//     return sum;
+// }
 
 int main()
 {
     cin >> N;
-    ll res = findSumOfDigits(N);
-    if (res % 9 == 0)
+    int len = strlen(N);
+    int sum = 0;
+    for (int i = 0; i < len; i++)
+    {
+        sum += N[i] - '0';
+    }
+
+    if (sum % 9 == 0)
         cout << "Yes" << endl;
     else
         cout << "No" << endl;
