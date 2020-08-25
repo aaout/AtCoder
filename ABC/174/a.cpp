e6f39359be9cd3bd612ffe16b1328958a8fca30d
aaout <tiktik_suke@outlook.jp>
2020-08-26 03:05:24 +0900

    ABC 174

---
+++ ABC/174/a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+ll X;
+
+//AC
+int main()
+{
+    cin >> X;
+    cout << (X >= 30 ? "Yes" : "No") << endl;
+}
---
+++ ABC/174/c_alpha.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+#define rep(i, n)                 \
+    for (int i = 0; i < (n); ++i) \
+        ;
+int K;
+
+int main()
+{
+    cin >> K;
+    int x = 7 % K;
+    set<int> s;
+    int i = 1;
+    while (s.count(x) == 0)
+    {
+        if (x == 0)
+        {
+            cout << i << endl;
+            return 0;
+        }
+        s.insert(x);
+        x = (x * 10 + 7) % K;
+        ++i;
+    }
+    cout << -1 << endl;
+}
