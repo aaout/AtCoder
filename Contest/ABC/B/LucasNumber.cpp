af3a0494480df608a43fb37784a0dbfe7a279c03
aaout <tiktik_suke@outlook.jp>
2020-08-31 11:34:35 +0900

    ok

---
+++ Contest/ABC/B/LucasNumber.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+//AC
+int main()
+{
+    ll n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << 1 << endl;
+        return 0;
+    }
+    ll count = 1;
+    ll Lb = 2, La = 1;
+    ll ans = 0;
+    while (count != n)
+    {
+        ans = Lb + La;
+        Lb = La;
+        La = ans;
+        count++;
+    }
+    cout << ans << endl;
+}
